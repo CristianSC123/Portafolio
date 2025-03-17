e8d7823dea8537a7d1e0573ba125c28172f7731a
student <ltnt315@gmail.com>
2025-03-17 11:24:56 -0400

    creando la carpeta de apps con cada uno de los modulos del portafolio personal

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.Certificado'.
+    'apps.Educacion',
+    'apps.Experiencia',
+    'apps.Infomacion',
+    'apps.Proyectos',
+    'apps.Skills'
 ]
 
 MIDDLEWARE = [
